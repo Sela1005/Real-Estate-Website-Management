358505ae22485e26d9bd151d0f36b100753be3d5
truc1 <truc15112002@gmail.com>
2024-11-08 10:59:41 +0700

    timkiem, dang ky

--- BatDongSan/Controllers/HomeController.cs
+++ BatDongSan/Controllers/HomeController.cs
@@ -30,7 +30,8 @@ namespace BatDongSan.Controllers
         public ActionResult XulyDangNhap(string username, string password)
         {
             DataModel db = new DataModel();
-            ViewBag.list = db.get("EXEC KIEMTRADANGNHAP '" + username + "','" + password + "'");
+            ViewBag.list = db.get("exec KIEMTRADANGNHAP '"+ username + "' ,'"+ password + "'");
+            
             if (ViewBag.list.Count > 0)
             {
                 Session["taikhoan"] = ViewBag.list[0];
@@ -51,10 +52,81 @@ namespace BatDongSan.Controllers
         
         public ActionResult Properties_Detail(string id, string idNguoiDung)
         {
-            DataModel db = new DataModel();
-            ViewBag.list = db.get("exec TIMKIEMBDSTHEOID " + id + ";");
-            ViewBag.listUser = db.get("exec TIMKIEMNguoiDungTHEOID " + idNguoiDung + ";");
+            if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(idNguoiDung) || !int.TryParse(id, out _) || !int.TryParse(idNguoiDung, out _))
+            {
+                return RedirectToAction("Index", "Home"); // Điều hướng về trang chủ nếu thiếu dữ liệu hoặc dữ liệu không hợp lệ
+            }
+            try
+            {
+                DataModel db = new DataModel();
+                ViewBag.list = db.get("exec TIMKIEMBDSTHEOID " + id + ";");
+                ViewBag.listUser = db.get("exec TIMKIEMNguoiDungTHEOID " + idNguoiDung + ";");
+            }
+            catch(Exception) { return RedirectToAction("Index", "Home"); }
             return View();
         }
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult XulyDangKy(string username, string password, string email, string confrimpassword)
+        {
+            DataModel db = new DataModel();
+
+            if(confrimpassword != password)
+            {
+                TempData["ErrorMessage"] = "Vui lòng nhập mật khẩu và Nhập lại mật khẩu giống nhau";
+                return RedirectToAction("Dangky", "Home"); // Chuyển đến action đăng ký
+            }
+
+            // Thực hiện thủ tục lưu trữ
+            ViewBag.list = db.get("EXEC SP_DangKy N'" + username + "', N'" + password + "', N'" + email + "'");
+
+            // Kiểm tra kết quả trả về
+            if (ViewBag.list.Count > 0)
+            {
+                int result = Convert.ToInt32(ViewBag.list[0][0]);
+
+                if (result == 1)
+                {
+                    // Đăng ký thành công
+                    return RedirectToAction("Dangnhap", "home");
+                }
+                else if (result == 0)
+                {
+                    TempData["ErrorMessage"] = "Email này đã tồn tại. Vui lòng thử lại với email khác.";
+                    return RedirectToAction("Dangky", "Home"); // Chuyển đến action đăng ký
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Đã xảy ra lỗi trong quá trình đăng ký. Vui lòng thử lại.";
+                    return RedirectToAction("Dangky", "Home");
+                }
+            }
+
+            TempData["ErrorMessage"] = "Đã xảy ra lỗi trong quá trình đăng ký. Vui lòng thử lại.";
+            return RedirectToAction("Dangky", "Home");
+        }
+        public ActionResult TimKiemTieuDeBDS(string TieuDe)
+        {
+            if (string.IsNullOrEmpty(TieuDe))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            try
+            {
+                DataModel db = new DataModel();
+                ViewBag.list = db.get("EXEC TIMKIEMBDSTHEOTIEUDE N'" + TieuDe + "'");
+            }
+            catch (Exception)
+            {
+                RedirectToAction("Index", "Home"); 
+            }
+            
+            return View();
+        }
+
+
     }
 }
